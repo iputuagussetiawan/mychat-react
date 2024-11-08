0cd3de1db11433e0076582126dce82cabfe60a01
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-11-08 23:20:46 +0800

    sign up layouting

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import {Button, Container, Paper, TextField, Typography} from '@mui/material'
+import {Avatar, Button, Container, IconButton, Paper, Stack, TextField, Typography} from '@mui/material'
+import {CameraAlt as CameraAltIcon} from '@mui/icons-material'
+import { VisuallyHiddenInput } from '../components/styles/StyledComponents'
 
 const Login = () => {
   const [isLogin,setIsLogin] = React.useState(true)
   const toggleLogin = () => setIsLogin(!isLogin);
   return (
     <>
-      <Container component={"main"} maxWidth={"xs"} sx={{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", p: 5}}>
+      <Container 
+        component={"main"} 
+        maxWidth={"xs"} 
+        sx={{
+          height: "100vh", 
+          display: "flex", 
+          justifyContent: "center", 
+          alignItems: "center", 
+          p: 5
+        }}>
         <Paper 
           elevation={2} 
           sx={{
@@ -64,7 +75,100 @@ const Login = () => {
                 </form>
               </>
             ) : (
-              <div>register</div>
+              <>
+              <Typography variant='h5'>Sign Up</Typography>
+                <form style={{
+                    width: "100%",
+                    marginTop: "1rem"
+                  }}>
+                  <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                  <Avatar
+                    sx={{
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                    
+                  />
+
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "white",
+                      bgcolor: "rgba(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgba(0,0,0,0.7)",
+                      },
+                    }}
+                    component="label"
+                  >
+                    <>
+                      <CameraAltIcon />
+                      <VisuallyHiddenInput
+                        type="file"
+                      />
+                    </>
+                  </IconButton>
+                </Stack>
+                  <TextField 
+                    required 
+                    fullWidth
+                    label='Name'
+                    name='name'
+                    margin='normal'
+                    variant='outlined'
+
+                  />
+                   <TextField 
+                    required 
+                    fullWidth
+                    label='Bio'
+                    name='bio'
+                    margin='normal'
+                    variant='outlined'
+
+                  />
+                  <TextField 
+                    required 
+                    fullWidth
+                    label='Username'
+                    name='username'
+                    margin='normal'
+                    variant='outlined'
+
+                  />
+                  <TextField 
+                    required 
+                    fullWidth
+                    label='Password'
+                    type='password'
+                    name='username'
+                    margin='normal'
+                    variant='outlined'
+                    
+                  />
+                  <Button 
+                    sx={{mt: "1rem"}}
+                    variant='contained' 
+                    color='primary' 
+                    type='submit'
+                    fullWidth
+                  >
+                    Sign Up
+                  </Button>
+                  <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+                  <Button 
+                    sx={{mt: "1rem"}}
+                    variant='text' 
+                    onClick={toggleLogin}
+                    fullWidth
+                  >
+                    Login
+                  </Button>
+                </form>
+            </>
             )
           }
         </Paper>
